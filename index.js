81751e57ee5bd8b65dbcabeae82801dee2234396
Samuel Stenton <sthstenton@gmail.com>
2016-06-25 23:43:19 +0100

    init

---
+++ index.js
@@ -0,0 +1,69 @@
+//Lets require/import the HTTP module
+var app = require('express')();
+var http = require('http').Server(app);
+
+const https = require('https');
+var io = require('socket.io')(http);
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('./database.sql');
+var fs = require('fs');
+
+io.on('connection', function(socket){
+    getVotes();
+  socket.on('event', function(data){});
+  socket.on('disconnect', function(){});
+});
+
+app.get('/', function(req, res){
+    res.sendfile('index.html');
+});
+
+http.listen(8080, function(){
+  console.log('listening on *:3000');
+});
+
+db.serialize(function() {
+  db.run("CREATE TABLE IF NOT EXISTS remain (votes integer, time datetime)");
+});
+
+var minutes = 0.1, the_interval = minutes * 60 * 1000;
+setInterval(function() {
+    var url = 'https://petition.parliament.uk/petitions/131215.json';
+
+    https.get(url, function(res){
+        var body = '';
+
+        res.on('data', function(chunk){
+            body += chunk;
+        });
+
+        res.on('end', function(){
+            var response = JSON.parse(body);
+            db.serialize(function() {
+                var stmt = db.prepare("INSERT INTO remain VALUES (?, ?)");
+
+                stmt.run(response['data']['attributes']['signature_count'], new Date());
+                console.log(response['data']['attributes']['signature_count']);
+                stmt.finalize();
+            });
+
+
+        });
+
+        getVotes();
+    }).on('error', function(e){
+          console.log("Got an error: ", e);
+    });
+
+}, the_interval);
+
+function getVotes()
+{
+    db.get("SELECT votes FROM remain ORDER BY time DESC LIMIT 1", function(err, row) {
+        //Io Emit
+        io.emit('votes', row.votes);
+    });
+
+}
+
+//db.close();
