ec6154afb262b1b4b9057c2422bcbed6858e00bb
Samuel Stenton <sthstenton@gmail.com>
2016-07-01 23:37:32 +0100

    remove limit

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ function getVotes()
 
 function getData()
 {
-    db.all("SELECT votes, time FROM remain WHERE rowid%700 = 0 ORDER BY time ASC LIMIT 1000", function(err, object) {
+    db.all("SELECT votes, time FROM remain WHERE rowid%1000 = 0 ORDER BY time ASC", function(err, object) {
         var votes = [];
         var time = [];
         for( var obj in object ) {
